0630073131ae7d9ddab39b9d65e603ced20712fa
Etienne Pretorius <ettienne_pretorius@yahoo.co.uk>
2023-02-26 12:43:57 +0000

    - Added initial set of error codes for aquarium libraries.

---
+++ README.md
@@ -0,0 +1,6 @@
+# aquarium-sea-urchin
+
+![main](https://github.com/pretore/aquarium-sea-urchin/actions/workflows/cmake.yml/badge.svg?branch=main)
+
+Error codes for aquarium libraries. 
+
---
+++ include/sea-urchin.h
@@ -0,0 +1,17 @@
+#ifndef _SEA_URCHIN_SEA_URCHIN_H_
+#define _SEA_URCHIN_SEA_URCHIN_H_
+
+#define SEA_URCHIN_ERROR_OBJECT_IS_NULL                 1
+#define SEA_URCHIN_ERROR_OUT_IS_NULL                    2
+#define SEA_URCHIN_ERROR_ITEM_IS_NULL                   3
+#define SEA_URCHIN_ERROR_VALUE_IS_NULL                  4
+#define SEA_URCHIN_ERROR_OTHER_IS_NULL                  5
+#define SEA_URCHIN_ERROR_MEMORY_ALLOCATION_FAILED       6
+#define SEA_URCHIN_ERROR_IS_EMPTY                       7
+#define SEA_URCHIN_ERROR_END_OF_SEQUENCE                8
+#define SEA_URCHIN_ERROR_INDEX_IS_OUT_OF_BOUNDS         9
+#define SEA_URCHIN_ERROR_ITEM_IS_OUT_OF_BOUNDS          10
+#define SEA_URCHIN_ERROR_STREAM_IS_NULL                 11
+#define SEA_URCHIN_ERROR_FUNCTION_IS_NULL               12
+
+#endif /* _SEA_URCHIN_SEA_URCHIN_H_ */
